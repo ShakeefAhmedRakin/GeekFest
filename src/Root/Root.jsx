b317187d5f3ed4931d506aec20f92d0905bb16b7
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-10-09 03:13:06 +0600

    Added the footer.

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Root = () => {
   return (
@@ -9,6 +10,7 @@ const Root = () => {
       </div>
       <div>
         <Outlet></Outlet>
+        <Footer></Footer>
       </div>
     </>
   );
