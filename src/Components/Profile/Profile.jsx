cb3fc83c04dd82594f6b5b3cc4437f1be8c186af
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-10-09 21:14:51 +0600

    Adjusted profile form.

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -9,13 +9,15 @@ const Profile = () => {
       <div className="pt-16 bg-gradient-to-bl from-teal-400 via-violet-500 to-violet-800">
         <div className="container mx-auto px-2">
           <div className="flex justify-center py-24">
-            <div className="bg-gradient-to-bl from-white to-gray-300 p-6 rounded-xl h-[500px]">
+            <div className="bg-gradient-to-bl from-white to-gray-300 p-6 rounded-xl h-[500px] w-[400px]">
               <div className="flex-1 flex flex-col h-full">
-                <div className="flex justify-between gap-4 items-center">
-                  <img
-                    src={user?.photoURL}
-                    className="w-full object-cover h-[96px] flex-1 rounded-xl"
-                  />
+                <div className="flex justify-start gap-4 items-center">
+                  <div>
+                    <img
+                      src={user?.photoURL}
+                      className="w-[96px] object-cover h-[96px] flex-1 rounded-xl"
+                    />
+                  </div>
                   <h1 className="text-violet-600 font-bold text-4xl">
                     {user?.displayName}
                   </h1>
